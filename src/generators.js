ec1ded4da61a1a8a0a42803f54dd61aa8bc3fd3b
Mukesh Soni <mukeshsoni@gmail.com>
2015-02-12 17:36:31 +0530

    Added a couple of string generators

--- src/generators.js
+++ src/generators.js
@@ -4,6 +4,20 @@ var constants = require('./constants');
 var fun = () => true;
 var generators = {};
 
+/*
+ * String generators
+ */ 
+generators.byte = () => Math.floor(Math.random() * 256);
+generators.char = () => String.fromCharCode(generators.byte());
+generators.string = (size) => {
+	var randomString = '';
+
+	for(var i in _.range(_.random(0, size))) {
+		randomString += generators.char();
+	}
+	return randomString;
+};
+
 generators.int = () => {
 	return _.random(constants.MAX_INT * -1, constants.MAX_INT);
 };
--- test/generator_tests.js
+++ test/generator_tests.js
@@ -2,12 +2,32 @@ var gen = require('../src/generators');
 var _ = require('lodash');
 var expect = require('chai').expect;
 
-describe('generators', () => {
-	it('should generate random integers', () => {
+describe('string generators', () => {
+	it('should generate byte between 0 and 255', () => {
+		_.times(300, () => expect(gen.byte()).to.be.within(0, 255));
+	});
 
-		for(var i in _.range(10)) {
-			console.log(gen.int());
-			expect(_.isNumber(gen.int())).to.be.true;
-		}
+	it('should not generate random string greater than size', () => {
+		_.times(10, () => {
+			var size = Math.round(Math.random()*1000);
+			expect(gen.string(size)).to.have.length.of.at.most(size);
+		});
+	});
+
+	// this test can actually fail some times (once in a million or so)
+	it('should generate some strings of length less than size in 100 iterations', () => {
+		var size = Math.round(Math.random()*1000);
+		var generatedStrings = _.map(_.range(100), () => {
+			return gen.string(size);
+		});
+
+		// atleast one generated string has size less than size
+		expect(_.some(generatedStrings, (string) => (string.length < size))).to.be.true;
+	});
+});
+
+describe('integer generators', () => {
+	it('should generate random integers', () => {
+		_.times(10, () => expect(_.isNumber(gen.int())).to.be.true);
 	});
 });
