ac446e2b862bdb87eeb544cca3746f6bed560bf9
Mukesh Soni <mukeshsoni@gmail.com>
2015-02-26 20:06:39 +0530

    added a default value for size for arrayOf

--- src/generators/array.js
+++ src/generators/array.js
@@ -3,7 +3,7 @@ var utils = require('../utils');
 var arrayGens = {};
 arrayGens.arrayOf = (gen) => {
     // yeah, unreadble, but fun :)
-    return size => utils.range(utils.random(0, size)).map(i => gen(i));
+    return (size=10) => utils.range(utils.random(0, size)).map(i => gen(i));
 }
 
 module.exports = arrayGens;
