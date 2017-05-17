f8221d988bcc756b2e2078b5501789fd4a50f1db
Mukesh Soni <msoni@planview.com>
2017-05-17 18:06:51 +0530

    wrote some code for testing shrunk values from rose trees using generators

--- src/jssmartcheck.js
+++ src/jssmartcheck.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// require('babel/polyfill');
 var assert = require('assert');
 var gen = require('./generators');
 var {
@@ -24,6 +23,41 @@ var getErrorMessage = (numTests, fail) => {
     });
 };
 
+function shrinkVal(gen, val, prop, index, allArgs) {
+    if(gen.shrink && typeof gen.shrink === 'function') {
+        const shrinkList = gen.shrink(val)
+        // if there is a shrink list
+        const nextVal = shrinkList.next()
+        if(nextVal.done === false) {
+            return shrinkVal(nextVal.value, val, prop, index, allArgs)
+        } else {
+            if(f.apply(null, [...allArgs.slice(0, index), shrunken.value, ...allArgs.slice(index + 1)]) === true) {
+                const nextVal2 = shrinkList.next()
+                if(nextVal2.done === false) {
+                    return val
+                } else {
+                    return shrinkVal(gen, nextVal2.value, prop, index, allArgs)
+                }
+            } else {
+                return val
+            }
+        }
+    } else {
+        return val
+    }
+}
+
+function shrinkVals(gens, vals, prop) {
+    var shrunkenValues = vals
+    return vals.map((val, i) => {
+        shrunkenValues[i] = shrinkVal(gens[i], val, prop, i, shrunkenValues)
+        return shrunkenValues[i]
+    })
+}
+
+jssmartcheck.shrinkVals = shrinkVals
+jssmartcheck.shrinkVal = shrinkVal
+
 jssmartcheck.check = (f, times=100, seed=Math.random()*1000) => {
     jssmartcheck.seed = seed;
     assert(typeof f === 'function', 'check expects a property function');
@@ -33,7 +67,11 @@ jssmartcheck.check = (f, times=100, seed=Math.random()*1000) => {
             return ranGen(getSize(i));
         });
 
-        assert(f.apply(undefined, sampleValues) === true, getErrorMessage(i, sampleValues));
+        if(f.apply(undefined, sampleValues) === true) {
+            console.log({ result: true, numTests: times, seed: seed });
+        } else {
+            throw new Error(getErrorMessage(i, shrinkVals(forallGens, sampleValues, f)))
+        }
     }
 
     console.log({ result: true, numTests: times, seed: seed });
